490635505dc44f40f65d12cb86c9a5f857a21beb
victoryang <yangtao3601@126.com>
2017-11-28 13:44:16 +0800

    api server version 0.1

---
+++ src/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "reflect"
+
+  "github.com/gorilla/mux"
+  "github.com/urfave/negroni"
+)
+
+var ETCD_URL = ":2379"
+var TRAEFIK_PREFIX = "/traefik"
+var ETCDCTL = "etcdctl"
+var CURL = "curl"
+
+func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "Welcome to the home page!",reflect.TypeOf(r))
+  })
+
+  n := negroni.Classic() // Includes some default middlewares
+
+  // add recovery middleware 
+  n.Use(negroni.NewRecovery())
+
+  RegisterRequests(r)
+  /* add middlewares here, since router is the last one */
+  n.UseHandler(r)
+
+  http.ListenAndServe(":3000", n)
+  fmt.Printf("Web server exits")
+}
+
+func getETCDSserver () {
+  return 
+}
+
+func getBackendServerUrl (w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  w.WriteHeader(http.StatusOK)
+  fmt.Fprintf(w, "backend " + vars["backendid"] + " server " + vars["serverid"] + " url is:")
+
+}
+
+func RegisterRequests (r *mux.Router) {
+  r.HandleFunc("/traefik/backends/{backendid}/servers/{serverid}/url", getBackendServerUrl).Methods("GET")
+  r.HandleFunc("/traefik/backends/{backendid}/servers/{serverid}/weight", getBackendServerUrl).Methods("GET")
+}
